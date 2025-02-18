bc2a7c1c11a293f621de9426fbc68aaaf5322ddb
Nikita Yaroslavtsev <n.yaroslavtsev@atomgroup.io>
2025-02-18 13:48:50 +0300

    Moved project to 5.5 version

--- Source/MemoriesEditor.Target.cs
+++ Source/MemoriesEditor.Target.cs
@@ -8,7 +8,7 @@ public class MemoriesEditorTarget : TargetRules
 	public MemoriesEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "Memories" } );
 	}
